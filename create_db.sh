eca722b4850090b56e21d5eb3924904c7f0bd038
fastCode <info@nfinityllc.com>
2020-12-26 11:11:48 -0500

    postgres additions

--- create_db.sh
+++ create_db.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 set -e
 
-POSTGRES="psql --username gitpod"
+POSTGRES="psql -U gitpod"
 
 echo "Creating database: dvdrental"
 
 $POSTGRES <<EOSQL
-CREATE DATABASE dvdrental OWNER gitpod;
+CREATE DATABASE dvdrental;
+ALTER ROLE gitpod WITH password 'fastcode';
 EOSQL
 
 echo "Creating schema..."
-psql -d dvdrental -a -U gitpod -f /dvdrental.sql
+psql -d dvdrental -f /dvdrental.sql
+
+
+#RUN psql -U gitpod postgres -c 'create database dvdrental'
+
+# '/bin/sh -c "psql -U gitpod postgres -c 'create database dvdrental;'"'
+
+# /bin/bash -c "psql -U gitpod postgres -c 'create database dvdrental;'"
+
+#RUN "psql -h localhost -U gitpod -d dvdrental -f /dvdrental.sql;"
+
+#RUN psql -U gitpod postgres -c "ALTER ROLE gitpod WITH password 'fastcode'"
+
+#SHELL ["/bin/sh", "-c"] 
+
+#RUN psql -U gitpod postgres -c "create database dvdrental;"
